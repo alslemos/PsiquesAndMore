adbdfc6ad5e88f7f8551273972e4511d26565312
alslemos <alexandre-ls@hotmail.com>
2023-10-16 21:06:29 -0300

    Initial Commit

---
+++ PsiquesAndMore/PsiquesAndMoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PsiquesAndMoreApp.swift
+//  PsiquesAndMore
+//
+//  Created by Alexandre Lemos da Silva on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PsiquesAndMoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
