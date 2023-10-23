9a019b78937baea843219f01aafee6da94fcc225
alslemos <alexandre-ls@hotmail.com>
2023-10-23 16:11:48 -0300

    agora branch dev

--- PsiquesAndMore/ContentView.swift
+++ PsiquesAndMore/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Psiques!")
+            Text("Agora na dev!")
         }
         .padding()
     }
