59925cf7a5f8fe2b5f7d636f533d2ee08b5d0d6c
alslemos <alexandre-ls@hotmail.com>
2023-10-25 21:15:18 -0300

    Started the tk12, 13 and 14

---
+++ PsiquesAndMore/Extensions/NotificationCenter+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationCenter+Extensions.swift
+//  PsiquesAndMore
+//
+//  Created by Alexandre Lemos da Silva on 25/10/23.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static var restartGameNotificationName: NSNotification.Name = NSNotification.Name("fechar")
+}
---
+++ PsiquesAndMore/FirstView.swift
@@ -0,0 +1,55 @@
+//
+//  FirstView.swift
+//  PsiquesAndMore
+//
+//  Created by Alexandre Lemos da Silva on 25/10/23.
+//
+
+import Foundation
+
+
+struct MainView: View {
+    
+    let publi =  NotificationCenter.default.publisher(for: .restartGameNotificationName)
+    
+    var scene: SKScene {
+        let scene = GameScene(size: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        return scene
+    }
+    
+    @State var showDetails: Bool = false
+    
+    var body: some View {
+        
+        ZStack {
+            Color(red: 13 / 255, green: 43 / 255, blue: 69 / 255)
+            
+            NavigationStack {
+                
+                VStack {
+                    Button {
+                        showDetails = true
+                    } label: {
+                        Image("playButton")
+                    }
+                }.onAppear{
+                    GKAccessPoint.shared.isActive = true
+                }
+                .onDisappear {
+                    GKAccessPoint.shared.isActive = false
+                }
+                
+                .navigationDestination(isPresented: $showDetails) {
+                    VStack {
+                        SpriteView(scene: scene).ignoresSafeArea().navigationBarBackButtonHidden(true)
+                    }
+                }
+            }.ignoresSafeArea()
+             .onReceive(publi) { _ in
+                showDetails.toggle()
+            }
+        }
+    }
+}
+
+
